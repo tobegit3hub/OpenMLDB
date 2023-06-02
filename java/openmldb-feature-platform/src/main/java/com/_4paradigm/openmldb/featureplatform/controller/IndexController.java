f24ed3d0ada5b0875d7fd4bf35b6a96cd6c012a0
tobe <tobeg3oogle@gmail.com>
2023-06-02 17:12:06 +0800

    Load vue generate html for springboot index page

--- java/openmldb-feature-platform/src/main/java/com/_4paradigm/openmldb/featureplatform/controller/IndexController.java
+++ java/openmldb-feature-platform/src/main/java/com/_4paradigm/openmldb/featureplatform/controller/IndexController.java
@@ -1,16 +1,16 @@
 package com._4paradigm.openmldb.featureplatform.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/")
 class IndexController {
 
     @GetMapping
-    public String hello() {
+    public String index() {
         // TODO: Render the single page app
-        return "Hello, World!";
+        return "index.html";
     }
 }
