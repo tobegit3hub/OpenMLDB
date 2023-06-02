159fd95ab8fe61a4a79ec3d3e41fc1aff2e2948b
tobe <tobeg3oogle@gmail.com>
2023-06-02 16:53:43 +0800

    Add vue project and entities list component

---
+++ java/openmldb-feature-platform/frontend/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+
+import EntitiesList from "./components/EntitiesList.vue";
+
+</script>
+
+<template>
+
+<body>  
+  <div>
+    <EntitiesList />
+  </div>
+</body>
+
+</template>
+
+<style scoped>
+</style>
---
+++ java/openmldb-feature-platform/frontend/src/components/EntitiesList.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <h2>Entities List</h2>
+      <ul>
+        <li v-for="entity in entities" :key="entity.name">
+          Name: {{ entity.name }}, Primary keys: {{ entity.primaryKeys }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+<script>
+  export default {
+    data() {
+      return {
+        entities: [],
+      };
+    },
+    mounted() {
+      this.fetchEntities();
+    },
+    methods: {
+      fetchEntities() {
+        fetch("http://127.0.0.1:8888/api/entities")
+          .then((response) => response.json())
+          .then((data) => {
+            this.entities = data;
+          })
+          .catch((error) => {
+            console.error("Error fetching entities:", error);
+          });
+      },
+    },
+  };
+  </script>
+  
