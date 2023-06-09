e3b0b1e695838f38da281f7b5f0f9283f78050c2
tobe <tobeg3oogle@gmail.com>
2023-06-09 18:30:09 +0800

    Add home page

--- java/openmldb-feature-platform/frontend/src/components/HomePage.vue
+++ java/openmldb-feature-platform/frontend/src/components/HomePage.vue
@@ -1,16 +1,165 @@
 <script setup>
-
-import EntitiesData from "./EntitiesData.vue";
-
 </script>
 
 <template>
 
   <div>
-    <EntitiesData />
+    <br />
+    <h1>Welcome to OpenMLDB Feature Platform</h1>
+    <p class="description">This is the welcome page for the Feaure Platform with your OpenMLDB data.</p>
+
+    <br /><br />
+    <a-row>
+    <a-col :span="11">
+      <h2><router-link to='/entities'>Entities</router-link></h2>
+      <a-table :columns="entityColumns" :data-source="entities" />
+    </a-col>
+    
+    <a-col :span="1"></a-col>
+    <a-col :span="12">
+      <h2><router-link to='/features'>Features</router-link></h2>
+      <a-table :columns="featureColumns" :data-source="features" />
+    </a-col>
+
+    <a-col :span="11">
+      <h2><router-link to='/featureviews'>Feature Views</router-link></h2>
+      <a-table :columns="featureViewColumns" :data-source="featureviews" />
+    </a-col>
+
+    <a-col :span="1"></a-col>
+    <a-col :span="12">
+      <h2><router-link to='/featureservices'>Feature Services</router-link></h2>
+      <a-table :columns="featureServiceColumns" :data-source="featureservices" />
+    </a-col>
+  </a-row>
   </div>
 
 </template>
+
+<script>
+import axios from 'axios'
+
+  export default {
+    data() {
+      return {
+        entities: [],
+        features: [],
+        featureviews: [],
+        featureservices: [],
+
+        entityColumns: [{
+          title: 'Name',
+          dataIndex: 'name',
+          key: 'name',
+        },
+        {
+          title: 'PrimaryKeys',
+          dataIndex: 'primaryKeys',
+          key: 'primaryKeys',
+        }],
+
+        featureColumns: [{
+          title: 'Feature View',
+          dataIndex: 'featureViewName',
+          key: 'featureViewName',
+        },
+        {
+          title: 'Feature Name',
+          dataIndex: 'featureName',
+          key: 'featureName',
+        },
+        {
+          title: 'Type',
+          dataIndex: 'type',
+          key: 'type',
+        }],
+
+
+        featureViewColumns: [{
+          title: 'Name',
+          dataIndex: 'name',
+          key: 'name',
+        },
+        {
+          title: 'Entities',
+          dataIndex: 'entityNames',
+          key: 'entityNames',
+        },
+        {
+          title: 'SQL',
+          dataIndex: 'sql',
+          key: 'sql',
+        },
+        {
+          title: 'Features',
+          dataIndex: 'featureNames',
+          key: 'featureNames',
+        }],
+
+        featureServiceColumns: [{
+          title: 'Name',
+          dataIndex: 'name',
+          key: 'name',
+        },
+        {
+          title: 'Feature Views',
+          dataIndex: 'featureViewNames',
+          key: 'featureViewNames',
+        },
+        {
+          title: 'SQL',
+          dataIndex: 'sql',
+          key: 'sql',
+        },
+        {
+          title: 'Deployment',
+          dataIndex: 'deployment',
+          key: 'deployment',
+        }],
+
+      };
+    },
+    mounted() {
+      this.initData();
+  
+    },
+    methods: {
+      initData() {
+        axios.get(`/api/entities`)
+          .then(response => {
+            this.entities = response.data;
+          })
+          .catch(error => {
+            console.log(error.message);
+          });
+
+        axios.get(`/api/features`)
+          .then(response => {
+            this.features = response.data;
+          })
+          .catch(error => {
+            console.log(error.message);
+          });
+
+        axios.get(`/api/featureviews`)
+          .then(response => {
+            this.featureviews = response.data;
+          })
+          .catch(error => {
+            console.log(error.message);
+          });
+
+        axios.get(`/api/featureservices`)
+          .then(response => {
+            this.featureservices = response.data;
+          })
+          .catch(error => {
+            console.log(error.message);
+          });
+      },
+    },
+  };
+</script>
 
 <style scoped>
 </style>
